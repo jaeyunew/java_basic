4796fa8936ce174c6455ba9538070992f2307a6f
Kim Jae Yun <qkenr3958@gmail.com>
2025-07-23 12:49:06 +0900

    day01code_start

---
+++ src/ex01.java
@@ -0,0 +1,18 @@
+public class ex01 {
+
+    public static void main(String[] args) {
+
+          int num1, num2, num3, sum;
+
+          num1 = 12;
+          num2 = 13;
+          num3 = 14;
+
+         sum = num1 + num2 +num3;
+
+         System.out.println("10+11+12 = " + sum);
+         System.out.println(num1 + "+" + num2 + "+" + num3 + "=" + sum);
+         System.out.printf("%d + %d + %d = %d " ,num1,num2,num3,sum);
+    }
+
+}
