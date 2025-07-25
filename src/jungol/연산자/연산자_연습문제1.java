e90a819352188ef003c92c24c138a1508201121f
Kim Jae Yun <qkenr3958@gmail.com>
2025-07-25 17:50:00 +0900

    day03_practice

---
+++ src/jungol/연산자/연산자_연습문제1.java
@@ -0,0 +1,17 @@
+package jungol.연산자;
+
+import java.util.Scanner;
+
+public class 연산자_연습문제1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("두 개의 수를 입력하시오.");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.printf("%d + %d = %d\n", a, b, a+b);
+        System.out.printf("%d - %d = %d\n", a, b, a-b);
+        System.out.printf("%d * %d = %d\n", a, b, a*b);
+        System.out.printf("%d / %d = %d\n", a, b, a/b);
+        System.out.printf("%d %% %d = %d\n", a, b, a%b);
+    }
+}
