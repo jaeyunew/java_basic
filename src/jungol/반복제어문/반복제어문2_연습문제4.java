70bfd4977d0dcfbe7ecc02cc4a2e3a18fdde5b99
Kim Jae Yun <qkenr3958@gmail.com>
2025-07-25 17:36:06 +0900

    day03_practice

---
+++ src/jungol/반복제어문/반복제어문2_연습문제4.java
@@ -0,0 +1,17 @@
+package jungol.반복제어문;
+
+import java.util.Scanner;
+
+public class 반복제어문2_연습문제4 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int sum = 0;
+
+        for (int i = 1; i <= a; i += 1) {
+            sum = sum + i;
+        }
+
+        System.out.println(sum);
+    }
+}
