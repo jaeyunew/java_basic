c3b8b7f465a6cd53dc1ecb0fbf1748aebb664451
Kim Jae Yun <qkenr3958@gmail.com>
2025-07-28 09:15:46 +0900

    day03_homework

--- src/jungol/연산자/연산자_연습문제1.java
+++ src/jungol/연산자/연산자_연습문제1.java
@@ -8,10 +8,10 @@ public class 연산자_연습문제1 {
         System.out.println("두 개의 수를 입력하시오.");
         int a = sc.nextInt();
         int b = sc.nextInt();
-        System.out.printf("%d + %d = %d\n", a, b, a+b);
-        System.out.printf("%d - %d = %d\n", a, b, a-b);
-        System.out.printf("%d * %d = %d\n", a, b, a*b);
-        System.out.printf("%d / %d = %d\n", a, b, a/b);
-        System.out.printf("%d %% %d = %d\n", a, b, a%b);
+        System.out.printf("%d + %d = %d\n", a, b, a + b);
+        System.out.printf("%d - %d = %d\n", a, b, a - b);
+        System.out.printf("%d * %d = %d\n", a, b, a * b);
+        System.out.printf("%d / %d = %d\n", a, b, a / b);
+        System.out.printf("%d %% %d = %d\n", a, b, a % b);
     }
 }
--- src/jungol/연산자/연산자_연습문제5.java
+++ src/jungol/연산자/연산자_연습문제5.java
@@ -2,16 +2,13 @@ package jungol.연산자;
 
 import java.util.Scanner;
 
-public class 연산자_연습문제2 {
+public class 연산자_연습문제5 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("5개의 수를 입력하시오.");
         int a = sc.nextInt();
         int b = sc.nextInt();
         int c = sc.nextInt();
-        int d = sc.nextInt();
-        int e = sc.nextInt();
 
-        System.out.printf("%d %d %d %d %d ", a + 3, b - 2, c * 3, d / 3, e % 3);
+        System.out.printf("%d %d %d %d", a == b ? 1 : 0, b == c ? 1 : 0, a != b ? 1 : 0, b != c ? 1 : 0);
     }
 }
