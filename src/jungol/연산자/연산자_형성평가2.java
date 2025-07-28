48142a8fc4554e1913abcd7bfd807ebeacc881f7
Kim Jae Yun <qkenr3958@gmail.com>
2025-07-28 10:00:49 +0900

    day03_homework

---
+++ src/jungol/연산자/연산자_형성평가2.java
@@ -0,0 +1,12 @@
+package jungol.연산자;
+
+import java.util.Scanner;
+
+public class 연산자_형성평가2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.printf("%d / %d = %d ... %d", a, b, a / b, a % b);
+    }
+}
