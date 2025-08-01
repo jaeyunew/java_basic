528975f4b8aba1c5856b820e9614582689ff1d6c
Kim Jae Yun <qkenr3958@gmail.com>
2025-08-01 15:34:49 +0900

    day08_practice

---
+++ src/jungol/문자열/문자열1_연습문제3.java
@@ -0,0 +1,21 @@
+package jungol.문자열;
+
+import java.util.Scanner;
+
+public class 문자열1_연습문제3 {
+    static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+
+        String str = "jungol olympiad";
+        int[] index = new int[5];
+
+        for (int i = 0; i < index.length; i++) {
+            index[i] = in.nextInt();
+        }
+        for (int i = 0; i < index.length; i++) {
+            System.out.print(str.charAt(index[i]));
+        }
+    }
+}
---
+++ src/jungol/문자열/문자열1_연습문제6.java
@@ -0,0 +1,27 @@
+package jungol.문자열;
+
+import java.util.Scanner;
+
+public class 문자열1_연습문제6 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+
+        // isDigit() 나 isUpperCase() 사용가능
+        while (true) {
+            char c = in.next().charAt(0);
+
+            if ((int) c >= 65 && (int) c <= 90) {
+                System.out.println("대문자입니다.");
+            } else if ((int) c >= 97 && (int) c <= 122) {
+                System.out.println("소문자입니다.");
+            } else if ((int) c >= 48 && (int) c <= 57) {
+                System.out.println("숫자문자입니다.");
+            } else {
+                System.out.println("영문, 숫자 이외의 문자입니다.");
+                break;
+            }
+
+        }
+    }
+}
