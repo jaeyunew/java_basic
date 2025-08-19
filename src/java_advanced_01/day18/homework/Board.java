7bb127e19a14b31f6b6bb05a64aea58612096df8
jaeyunew <qkenr3958@gmail.com>
2025-08-19 23:07:38 +0900

    homework

--- src/java_advanced_01/day18/homework/Board.java
+++ src/java_advanced_01/day18/homework/Board.java
@@ -7,6 +7,27 @@ import java.util.Date;
 @Data
 public class Board {
     private int bno;       //글번호
+
+    public int getBno() {
+        return bno;
+    }
+
+    public String getbTitle() {
+        return bTitle;
+    }
+
+    public String getbContent() {
+        return bContent;
+    }
+
+    public String getbWriter() {
+        return bWriter;
+    }
+
+    public Date getbDate() {
+        return bDate;
+    }
+
     private String bTitle;  //글제목
     private String bContent; //글내용
     private String bWriter;  //글쓴이
