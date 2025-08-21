dfe334a79a53568111a3218a957f443b36dc2dfb
Kim Jae Yun <qkenr3958@gmail.com>
2025-08-22 08:51:01 +0900

    homework

---
+++ src/java_advanced_01/day19/homework/Example02.java
@@ -0,0 +1,20 @@
+package java_advanced_01.day19.homework;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Example02 {
+    public static void main(String[] args) {
+        List<Member02> list = Arrays.asList(
+                new Member02("홍길동", "개발자"),
+                new Member02("김나리", "디자이너"),
+                new Member02("신용권", "개발자")
+        );
+
+        List<Member02> developers = list.stream()
+                .filter(job -> job.getJob().equals("개발자")).toList();
+
+        developers.forEach(job -> System.out.println(job.getName()));
+    }
+}
---
+++ src/java_advanced_01/day19/homework/Member02.java
@@ -0,0 +1,14 @@
+package java_advanced_01.day19.homework;
+
+public class Member02 {
+    private String name;
+    private String job;
+
+    public Member02(String name, String job) {
+        this.name = name;
+        this.job = job;
+    }
+
+    public String getName() { return name; }
+    public String getJob() { return job; }
+}
